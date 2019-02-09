677022bbc19c0b0d4916b85e707e10314cdb6cb7
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-09 12:40:30 -0800

    implementing initial calculations

--- bottles.js
+++ bottles.js
@@ -3,3 +3,20 @@
 // Each bottle of pop costs $2 to purchase
 // Given these parameters, write a program so that you can figure out how 
 // many total bottles of pop can be redeemed given a customer investment.
+const money = process.argv[2];
+const price = 2;
+var bottles = 0;
+var caps = 0;
+
+// returns how many full bottles user gets
+function moneyStuff() {
+  let bottlesTemp = (Math.trunc(money / price));
+  money -= bottlesTemp * price;
+  return bottlesTemp;
+}
+
+// returns how many full bottles user gets from their empties
+function bottleCalc() {}
+
+// returns how many full bottles user gets from their caps
+function capCalc() {}
