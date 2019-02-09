10421134b8650fed8408494c0ac820f17a1e3ec0
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-09 13:21:58 -0800

    added implementation, needs to work recursively probably

--- bottles.js
+++ bottles.js
@@ -3,20 +3,68 @@
 // Each bottle of pop costs $2 to purchase
 // Given these parameters, write a program so that you can figure out how 
 // many total bottles of pop can be redeemed given a customer investment.
-const money = process.argv[2];
 const price = 2;
-var bottles = 0;
+const emptyToFull = 2;
+const capsToFull = 4;
+
+var money = process.argv[2];
+var fullBottles = {
+  total: 0,
+  fromEmpties: 0,
+  fromCaps: 0
+};
+var emptyBottles = 0;
 var caps = 0;
+
+doTheStuff();
+
+// runs through the calcs and writes to log the expected out
+function doTheStuff() {
+  fullBottles.total += moneyStuff();
+  fullBottles.total += bottleCalc();
+  fullBottles.total += capCalc();
+
+  console.log(`Total bottles: ${fullBottles.total}\n` +
+    `Remaining bottles: ${emptyBottles}\n` +
+    `Remaining caps: ${caps}\n` +
+    `Total earned:\n` +
+    `  Bottles: ${fullBottles.fromEmpties}\n` +
+    `  Caps: ${fullBottles.fromCaps}`);
+}
 
 // returns how many full bottles user gets
 function moneyStuff() {
   let bottlesTemp = (Math.trunc(money / price));
+
   money -= bottlesTemp * price;
+  emptyBottles += bottlesTemp;
+  caps += bottlesTemp;
+
   return bottlesTemp;
 }
 
 // returns how many full bottles user gets from their empties
-function bottleCalc() {}
+function bottleCalc() {
+  let bottlesTemp = Math.trunc(emptyBottles / emptyToFull);
+  emptyBottles -= bottlesTemp * emptyToFull;
+
+  fullBottles.fromEmpties = bottlesTemp;
+  return bottlesTemp;
+}
 
 // returns how many full bottles user gets from their caps
-function capCalc() {}
+function capCalc() {
+  let capsTemp = Math.trunc(caps / capsToFull);
+  caps -= capsTemp * capsToFull;
+
+  fullBottles.fromCaps = capsTemp;
+  return capsTemp;
+}
+
+// *** Sample output ***
+// Total bottles: 75
+// Remaining bottles: 1
+// Remaining caps: 3
+// Total earned:
+//   Bottles: 37
+//   Caps: 18
